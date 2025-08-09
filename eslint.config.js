1f8ac62a7b51ec09d1c63c2a0e2e57ae3c0985d3
binayachaudari <binayachaudari@live.com>
2025-08-09 11:05:35 -0400

    project setup

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: { 'jsx-a11y': jsxA11y },
+    extends: [
+      js.configs.recommended,
+      'plugin:jsx-a11y/recommended',
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      'eslint-config-prettier',
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+])
