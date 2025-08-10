8bfc9c249618c2514d4b059ff34b55e6b31a3a2f
binayachaudari <binayachaudari@live.com>
2025-08-09 23:27:10 -0400

    feat: update App component to use dynamic card brand from usePin hook and integrate Outlet for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import ATMScreen from './components/ATMScreen'
+import { Outlet } from 'react-router-dom'
 import CardType from './components/CardType'
 import Logo from './components/Logo'
-import { CARD_BRAND } from './constants/cardBrands'
+import { usePin } from './hooks/usePin'
 
 function App() {
+  const { cardBrand } = usePin()
   return (
     <div className="flex flex-col items-center justify-end h-screen bg-[#9475a2]">
       <div className="flex justify-center items-center">
@@ -11,8 +12,8 @@ function App() {
       </div>
       <div className="bg-[#efeee3] w-100 h-3/4 border-t-10 border-[#b6b6b6]">
         <div className="flex flex-col items-center justify-center">
-          <CardType activeBrand={CARD_BRAND.VISA} />
-          <ATMScreen />
+          <CardType activeBrand={cardBrand} />
+          <Outlet />
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App.jsx';
+import { PinProvider } from './provider/PinProvider';
+import { router } from './routes/router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PinProvider>
+      <RouterProvider router={router} />
+    </PinProvider>
   </StrictMode>
 );
